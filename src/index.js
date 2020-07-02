434b723c9c5575b5e101364125efeb4a1fa18c6b
nomedia <EEXREPWDU.ZSUHBAX@my.tccd.edu>
2020-07-02 11:08:42 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,117 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Container, Row, Col, Card, MessageModalLayout, Checkbox, Text} from 'wix-style-react';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "wix-style-react";
 function text(children) {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                background: 'rgba(0,0,0,.1)',
-                width: '100%',
-                height: '50px',
-            }}
-            children={children}
-        />
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "rgba(0,0,0,.1)",
+        width: "100%",
+        height: "50px"
+      }}
+      children={children}
+    />
+  );
 }
-
 const App = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Button>Button</Button>
+        </Col>
+      </Row>
 
-    return (
-        <div>
-            <Container>
-                <Row>
+      <Row>
+        <Col xs={6}>{text("<Col span={6}/>")}</Col>
 
-                    <MessageModalLayout
-                        primaryButtonText="Leave"
-                        secondaryButtonText="Cancel"
-                        title="Discard changes"
-                        sideActions={<Checkbox>Don't show this again</Checkbox>}
-                        onCloseButtonClick={() => {
-                        }}
-                    >
-                        <Text>
-                            Are you sure you want to leave this page? Your changes won't be saved.
-                        </Text>
-                    </MessageModalLayout>
-                    <Col span={8}>
-                        <Card>
-                            <Card.Header subtitle="Card Subtitle" title="Card Header"/>
-                            <Card.Divider/>
-                            <Card.Content>
-                                <Container fluid>
-                                    <Row>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                    </Row>
+        <Col xs={6}>{text("<Col span={6}/>")}</Col>
+      </Row>
 
-                                    <Row>
-                                        <Col span={6}>{text('<Col span={6}/>')}</Col>
-                                    </Row>
+      <Row>
+        <Col xs={8}>{text("<Col span={8}/>")}</Col>
 
-                                    <Row>
-                                        <Col span={6}>{text('<Col span={6}/>')}</Col>
-                                        <Col span={3}>{text('<Col span={3}/>')}</Col>
-                                        <Col span={3}>{text('<Col span={3}/>')}</Col>
-                                    </Row>
-                                </Container>
-                            </Card.Content>
-                        </Card>
-                    </Col>
+        <Col xs={4}>{text("<Col span={4}/>")}</Col>
+      </Row>
 
-                    <Col span={4}>
-                        <Card>
-                            <Card.Header title="Side Card"/>
-                            <Card.Divider/>
-                            <Card.Content>
-                                <Container fluid>
-                                    <Row>
-                                        <Col span={12}>{text('<Col span={12}/>')}</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col span={6}>{text('<Col span={6}/>')}</Col>
-                                        <Col span={6}>{text('<Col span={6}/>')}</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col span={12}>{text('<Col span={12}/>')}</Col>
-                                    </Row>
-                                </Container>
-                            </Card.Content>
-                        </Card>
-                    </Col>
-                </Row>
+      <Row>
+        <Col xs={6}>{text("<Col span={6}/>")}</Col>
 
-                <Row>
-                    <Col span={12}>
-                        <Card>
-                            <Card.Header title="Full width Card"/>
-                            <Card.Divider/>
-                            <Card.Content>
-                                <Container fluid>
-                                    <Row>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                        <Col span={4}>{text('<Col span={4}/>')}</Col>
-                                    </Row>
-                                </Container>
-                            </Card.Content>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+        <Col xs={3}>{text("<Col span={3}/>")}</Col>
+
+        <Col xs={3}>{text("<Col span={3}/>")}</Col>
+      </Row>
+    </Container>
+  );
 };
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 console.log(root);
 
-ReactDOM.render(
-    <App/>,
-    root,
-);
-
+ReactDOM.render(<App />, root);
